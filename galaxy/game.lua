ef15c80484807965f11f57fbb19723e202c3dc24
angus <446070848@qq.com>
2017-07-14 15:59:56 +0800

    enter

--- galaxy/game.lua
+++ galaxy/game.lua
@@ -74,6 +74,7 @@ function game:draw()
         end
      ----------------------------------------------------------------       
         love.graphics.print("ui object num:"..#self.ui_object,0,0)
+        love.graphics.print("Fps:"..love.timer.getFPS())
         love.graphics.print("main object:"..self.main_object.name,150,0)
         love.graphics.print((math.floor(self.main_object.locate_x) or "nil").." "
                                 ..(math.floor(self.main_object.locate_y) or "nil").." "
